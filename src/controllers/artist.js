39712c98000b843b0fd4500c4435041aadc4da4e
Steven Lomax <ste.lomax@gmail.com>
2022-05-23 23:24:40 +0100

    create artist passing now secured

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -4,12 +4,30 @@
 
 const getDb = require('../services/db');
 
-exports.create = async (req, res) => {
+// exports.create = async (req, res) => {
+//     const db = await getDb();
+//     const { name, genre } = req.body;
+  
+//     try {
+//       await db.query(`INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}')`);
+  
+//       res.sendStatus(201);
+//     } catch (err) {
+//       res.sendStatus(500).json(err);
+//     }
+  
+//     db.close();
+//   };
+
+  exports.create = async (req, res) => {
     const db = await getDb();
     const { name, genre } = req.body;
   
     try {
-      await db.query(`INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}')`);
+      await db.query('INSERT INTO Artist (name, genre) VALUES (?, ?)', [
+        name,
+        genre,
+      ]);
   
       res.sendStatus(201);
     } catch (err) {
