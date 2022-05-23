9a765a5d4275f7e36a49a301165394ff11cfba4a
Steven Lomax <ste.lomax@gmail.com>
2022-05-23 23:20:08 +0100

    create artist passing

--- src/app.js
+++ src/app.js
@@ -4,8 +4,12 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).json({ result: `Hello, world!` });
-});
+// app.get('/', (req, res) => {
+//   res.status(200).json({ result: `Hello, world!` });
+// });
+
+const artistRouter = require('./routes/artist');
+
+app.use('/artist', artistRouter);
 
 module.exports = app;
